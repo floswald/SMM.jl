aed4676851753d3138981728e1b8ff44a03a3d73
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2014-10-02 10:08:06 -0500

    added methods to Eval type

--- src/mopt/Eval.jl
+++ src/mopt/Eval.jl
@@ -1,6 +1,9 @@
 # We introduce two bojects to facilitate the interfacing
 # with the objective function: Eval and Opts.
 
+export Eval, start, finish, param, fill, dataMoment, dataMomentsW, setMoment, setValue
+
+
 type Eval
 
 	value  ::Float64
@@ -8,7 +11,9 @@ type Eval
 	params ::Dict
 	moments::Dict
 	dataMoments::Dict
+	dataMomentsW::Dict
 	status ::Int64
+	options :: Dict
 
 	function Eval(p::Dict,mom::DataFrame)
 		this = new()
@@ -27,3 +32,44 @@ end
 function finish(ev::Eval)
 	ev.time = ev.time - time()
 end
+
+param(ev::Eval,ll::Array{Any,1})      = [ ev.params[i] for i in ll]
+param(ev::Eval)                       = param(ev::Eval,keys(ev.params))
+dataMoment(ev::Eval,ll::Array{Any,1}) = [ ev.dataMoments[i] for i in ll]
+dataMoment(ev::Eval)                  = dataMoment(ev::Eval,keys(ev.dataMoments))
+dataMomentW(ev::Eval,ll::Array{Any,1}) = [ ev.dataMomentsW[i] for i in ll]
+dataMomentW(ev::Eval)                  = dataMoment(ev::Eval,keys(ev.dataMomentsW))
+
+# this allows to fill the values of a given structure
+# with the values from ev
+function fill(p::Any,ev::Eval)
+	for k in keys(ev.params)
+		setfield(p,k,ev.params[k])
+	end
+end
+
+function setValue(ev::Eval,value::Float64)
+	ev.value = value
+end
+
+function setMoment(ev::Eval,k::Symbol,value::Float64)
+	ev.moments[k] = value
+end
+
+function setMoment(ev::Eval,d::Dict)
+	for k in keys(d)
+		ev.moments[k] = d[k]
+	end
+end
+
+# this assumes that colum :name has the names as strings
+# and that column :value stores the value
+function setMoment(ev::Eval,d::DataFrame)
+	for k in 1:nrows(d)
+		ev.moments[ symbol(d[i,:name]) ] = d[i,:value]
+	end
+end
+
+
+
+
