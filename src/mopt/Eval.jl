0d6b83cc04418c9e15c277bc4fd8c1485dd6185c
florian oswald <florian.oswald@gmail.com>
2016-05-31 22:16:07 +0200

    invert difference

--- src/mopt/Eval.jl
+++ src/mopt/Eval.jl
@@ -214,7 +214,7 @@ function check_moments(ev::Eval)
 	d = DataFrame(moment = collect(keys(ev.dataMoments)),data = collect(values(ev.dataMoments)))
 	dsim = DataFrame(moment = collect(keys(ev.simMoments)),simulation= collect(values(ev.simMoments)))
 	r = join(d,dsim, on=:moment)
-	r[:distance] = r[:data] .- r[:simulation]
+	r[:distance] =  r[:simulation] .- r[:data]
 	r[:percent_dev] = 100 * r[:distance] ./ r[:data]
 
 	return r
